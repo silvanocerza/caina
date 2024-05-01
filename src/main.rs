1f1aa0c16825a63a042ff3cef8c9e3bd2b6a0331
Silvano Cerza <silvanocerza@gmail.com>
2024-05-01 11:55:23 +0200

    Change peer id prefix

--- src/main.rs
+++ src/main.rs
@@ -115,9 +115,9 @@ fn generate_peer_id() -> String {
         .take(12)
         .map(|b| format!("%{:02X}", b))
         .collect();
-    // Use CC000 as prefix, CC from Cascata, the name I'll be using for the project
+    // Use CN000 as prefix, CN from Caina, the name I'll be using for the project
     // 000 is the version, just zeroes for the time being.
-    let peer_id_fixed: String = "-CC0000-".bytes().map(|b| format!("%{:02X}", b)).collect();
+    let peer_id_fixed: String = "-CN0000-".bytes().map(|b| format!("%{:02X}", b)).collect();
     format!("{}{}", peer_id_fixed, random_id_suffix)
 }
 
