00723b80765860cfb97417118e7e508fb6f517d7
Silvano Cerza <silvanocerza@gmail.com>
2024-05-03 14:20:06 +0200

    First draft connecting to peers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use core::panic;
-use std::{net::IpAddr, path::PathBuf};
+use std::{
+    collections::HashMap,
+    io::{Read, Write},
+    net::{IpAddr, TcpStream},
+    path::PathBuf,
+};
 
+use bincode::Options;
 use clap::Parser;
 use rand::{distributions::Alphanumeric, Rng};
 use serde::{Deserialize, Deserializer};
@@ -54,9 +60,19 @@ impl Info {
                 panic!("Failed parsing: {}", err)
             }
         };
+        Sha1::digest(buf).iter().map(|b| format!("{}", b)).collect()
+    }
+
+    fn hash_encoded(&self) -> String {
+        let buf = match serde_bencode::to_bytes(self) {
+            Ok(buf) => buf,
+            Err(err) => {
+                panic!("Failed parsing: {}", err)
+            }
+        };
         Sha1::digest(buf)
             .iter()
-            .map(|b| format!("%{:02X}", b))
+            .map(|b: &u8| format!("%{:02X}", b))
             .collect()
     }
 
@@ -96,6 +112,13 @@ struct Peer {
     ip: String,
     port: String,
 }
+
+impl Peer {
+    fn address(&self) -> String {
+        format!("{}:{}", self.ip, self.port)
+    }
+}
+
 fn deserialize_peers<'de, D>(deserializer: D) -> Result<Vec<Peer>, D::Error>
 where
     D: Deserializer<'de>,
@@ -154,27 +177,27 @@ fn generate_peer_id() -> String {
     format!("{}{}", peer_id_fixed, random_id_suffix)
 }
 
-fn build_tracker_url(torrent: &MetaInfo, peer_id: String) -> String {
+fn build_tracker_url(torrent: &MetaInfo, peer_id: &String) -> String {
     format!(
         "{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&compact={}&left={}",
         torrent.announce,
-        torrent.info.hash(),
+        torrent.info.hash_encoded(),
         peer_id,
         "6881",
         "0",
         "0",
-        "0",
+        "1",
         torrent.info.size(),
     )
 }
 
-fn tracker_get(torrent: &MetaInfo, peer_id: String) -> Result<TrackerResponse, String> {
+fn tracker_get(torrent: &MetaInfo, peer_id: &String) -> Result<TrackerResponse, String> {
     if !torrent.announce.starts_with("http") {
         // TODO: Support UDP trackers
         let protocol = torrent.announce.split(":").collect::<Vec<&str>>()[0];
         panic!("{} trackers not supported", protocol)
     }
-    let url = build_tracker_url(torrent, peer_id);
+    let url = build_tracker_url(torrent, &peer_id);
     let client = reqwest::blocking::Client::new();
     let res = match client.get(url).send() {
         Ok(res) => res,
@@ -197,6 +220,102 @@ fn tracker_get(torrent: &MetaInfo, peer_id: String) -> Result<TrackerResponse, S
     }
 
     Ok(tracker_res)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Handshake<'a> {
+    #[serde(rename = "pstrlen")]
+    protocol_string_length: u8,
+    #[serde(rename = "pstr")]
+    protocol: &'a str,
+    #[serde(default, with = "serde_bytes")]
+    reserved: [u8; 8],
+    #[serde(default)]
+    info_hash: &'a str,
+    #[serde(default)]
+    peer_id: &'a str,
+}
+
+impl Handshake<'_> {
+    fn new<'a>(info_hash: &'a String, peer_id: &'a String) -> Handshake<'a> {
+        Handshake {
+            protocol_string_length: 19,
+            protocol: "BitTorrent protocol",
+            reserved: [0; 8],
+            info_hash: info_hash.as_str(),
+            peer_id: peer_id.as_str(),
+        }
+    }
+}
+
+fn open_stream(peer: &Peer, info_hash: &String, peer_id: &String) -> Result<TcpStream, String> {
+    // let timeout = Duration::new(3, 0);
+    // let mut stream = match TcpStream::connect_timeout(peer.address(), timeout) {
+    let mut stream = match TcpStream::connect(peer.address()) {
+        Ok(s) => s,
+        Err(err) => return Err(format!("Failed opening TCP stream: {}", err)),
+    };
+
+    // Handshake
+    let handshake = Handshake::new(info_hash, peer_id);
+
+    // let mut data: Vec<u8> = vec![];
+    // data.push(handshake.protocol_string_length);
+    // data.extend_from_slice(handshake.protocol.as_bytes());
+    // data.extend_from_slice(&handshake.reserved[..]);
+    // data.extend_from_slice(handshake.info_hash.as_bytes());
+    // data.extend_from_slice(handshake.peer_id.as_bytes());
+
+    let options = bincode::DefaultOptions::new();
+    // .with_big_endian()
+    // .allow_trailing_bytes()
+    // .with_fixint_encoding();
+
+    let handshake = match options.serialize(&handshake) {
+        Ok(b) => b,
+        Err(err) => return Err(format!("Failed serializing handshake: {}", err)),
+    };
+
+    match stream.write(&handshake) {
+        Ok(size) => println!("Sent {} bytes", size),
+        Err(err) => return Err(format!("Failed sending handshake: {}", err)),
+    };
+
+    // We're using 68 as the size here cause we know the exact length of the handshake message, that is:
+    // protocol_string_length: 1 bytes
+    // protocol: 19 bytes
+    // reserved: 8 bytes
+    // info_hash: 20 bytes
+    // peer_id: 20 bytes
+    // This is not completely reliable as we relying on the fact the protocol is string is exactly
+    // 19 bytes long, but it could not be. This is good enough for the time being.
+    let mut buf = [0; 300];
+
+    match stream.read(&mut buf[..]) {
+        Ok(size) => println!("Received {} bytes", size),
+        Err(err) => return Err(format!("Failed reading data from peer: {}", err)),
+    };
+
+    println!("sent     bytes: {:?}", handshake);
+    println!("received bytes: {:?}", buf);
+
+    let handshake: Handshake = match options.deserialize(&buf) {
+        Ok(res) => res,
+        Err(err) => return Err(format!("Failed deserializing handshake: {}", err)),
+    };
+    if handshake.info_hash != info_hash {
+        // This is not the file we want, there's something wrong.
+        // Close the connection.
+        _ = stream.shutdown(std::net::Shutdown::Both);
+        return Err(String::from("Received wrong info hash from peer"));
+    }
+    if peer_id.len() > 0 && peer_id != handshake.peer_id {
+        // This peer is returning a different id than expected.
+        // Close the connection.
+        _ = stream.shutdown(std::net::Shutdown::Both);
+        return Err(String::from("Received unexpected peer id from peer"));
+    }
+    Ok(stream)
 }
 
 #[derive(Parser)]
@@ -244,8 +363,23 @@ fn main() {
         }
     }
 
-    let tracker_response = match tracker_get(&res, peer_id) {
+    let tracker_response = match tracker_get(&res, &peer_id) {
         Ok(res) => res,
         Err(err) => panic!("{}", err),
     };
+
+    let mut streams = HashMap::new();
+    for peer in tracker_response.peers {
+        println!("Trying to connect to {}", peer.address());
+        let stream = match open_stream(&peer, &res.info.hash(), &peer_id) {
+            Ok(s) => s,
+            Err(err) => {
+                println!("Couldn't connect with peer: {:?}", err);
+                continue;
+            }
+        };
+        streams.insert(peer.address(), stream);
+
+        println!("Opened stream with {}", peer.address());
+    }
 }
